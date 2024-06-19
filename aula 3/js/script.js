50ed0f8c32b75f0276ccf6df8eb299dfa43de6c6
Matheus <math2306.r@gmail.com>
2024-06-19 08:09:12 -0300

    termino de revisão do html

---
+++ aula 3/js/script.js
@@ -0,0 +1,14 @@
+trocatema = () => {
+  // alert("troca tema")
+  // const html =document.documentElement
+  // document.querySelector("body").style.background="pink"
+
+  document.documentElement.classList.toggle("light");
+
+  const isLight = document.documentElement.classList.contains("light");
+
+  const imagem = isLight ? "./img/perfil.png" : "./img/OIP.jpg";
+
+  document.querySelector("#profile img").setAttribute("src", imagem);
+
+};
